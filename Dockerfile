53563f7f69784ae10dbde381c6855ab739ea42b7
Nextek <34786850+NNaso@users.noreply.github.com>
2023-06-22 00:44:56 -0700

    initial commit

---
+++ Dockerfile
@@ -0,0 +1,57 @@
+FROM alpine:latest AS td_build
+
+ARG OPENTTD_VERSION="jgrpp-0.54.2"
+ARG OPENGFX_VERSION="7.1"
+
+RUN mkdir -p /config \
+    && mkdir /tmp/src
+
+# Install build dependencies
+RUN apk --no-cache add \
+    unzip \
+    wget \
+    git \
+    g++ \
+    make \
+    cmake \
+    patch \
+    xz-dev \
+    pkgconfig \
+    bash
+
+# Build OpenTTD itself
+WORKDIR /tmp/src
+
+RUN git clone https://github.com/JGRennison/OpenTTD-patches.git . \
+    && git fetch --tags \
+    && git checkout ${OPENTTD_VERSION}
+
+# Perform the build with the build script (1.11 switches to cmake, so use a script for decision making)
+ADD builder.sh /usr/local/bin/builder
+RUN chmod +x /usr/local/bin/builder && builder && rm /usr/local/bin/builder
+
+# Add the latest graphics files
+## Install OpenGFX
+RUN mkdir -p /app/data/baseset/ \
+    && cd /app/data/baseset/ \
+    && wget -q https://cdn.openttd.org/opengfx-releases/${OPENGFX_VERSION}/opengfx-${OPENGFX_VERSION}-all.zip \
+    && unzip opengfx-${OPENGFX_VERSION}-all.zip \
+    && tar -xf opengfx-${OPENGFX_VERSION}.tar \
+    && rm -rf opengfx-*.tar opengfx-*.zip
+
+
+FROM alpine:latest
+ARG OPENTTD_VERSION="jgrpp-0.54.2"
+RUN mkdir -p /usr/games/openttd/ \
+    && apk --no-cache add tini xz libstdc++ libgcc
+
+COPY --from=td_build /app /usr/games/openttd
+COPY --chown=1000:1000 --chmod=+x openttd.sh /openttd.sh
+RUN chmod +x /openttd.sh
+
+ENV PUID=1000
+ENV PGID=1000
+
+EXPOSE 3979/tcp 3979/udp
+
+ENTRYPOINT ["tini", "-vv", "--", "/openttd.sh"]
---
+++ openttd.sh
@@ -0,0 +1,108 @@
+#!/bin/sh
+
+set -o errexit
+set -o pipefail
+
+savepath="/home/openttd/.openttd/save"
+savegame="${savepath}/${savename}"
+LOADGAME_CHECK="${loadgame}x"
+loadgame=${loadgame:-'false'}
+execpath="/usr/games/openttd/openttd"
+
+PUID=${PUID:-911}
+PGID=${PGID:-911}
+PHOME=${PHOME:-"/home/openttd"}
+USER=${USER:-"openttd"}
+
+addgroup --gid "${PGID}" --system openttd
+adduser openttd --disabled-password --uid "${PUID}" -s /bin/sh -G openttd
+
+chmod +x $execpath
+
+if [ ! "$(id -u ${USER})" -eq "$PUID" ]; then usermod -o -u "$PUID" ${USER} ; fi
+if [ ! "$(id -g ${USER})" -eq "$PGID" ]; then groupmod -o -g "$PGID" ${USER} ; fi
+if [ "$(grep ${USER} /etc/passwd | cut -d':' -f6)" != "${PHOME}" ]; then
+        if [ ! -d ${PHOME} ]; then
+                mkdir -p ${PHOME}
+                chown ${USER}:${USER} ${PHOME}
+        fi
+        usermod -m -d ${PHOME} ${USER}
+fi
+
+mkdir -p ${savepath}
+chown ${USER}:${USER} -R ${savepath}
+
+#fix home folder permissions
+chown ${USER}:${USER} -R ${PHOME}
+
+
+echo "
+-----------------------------------
+GID/UID
+-----------------------------------
+User uid:    $(id -u ${USER})
+User gid:    $(id -g ${USER})
+User Home:   $(grep ${USER} /etc/passwd | cut -d':' -f6)
+-----------------------------------
+"
+
+# Loads the desired game, or prepare to load it next time server starts up!
+if [ ${LOADGAME_CHECK} != "x" ]; then
+
+        case ${loadgame} in
+                'true')
+                        if [ -f  ${savegame} ]; then
+                                echo "We are loading a save game!"
+                                echo "Lets load ${savegame}"
+                                su -l openttd -c "$execpath -D -g ${savegame} -x -d ${DEBUG}"
+                                exit 0
+                        else
+                                echo "${savegame} not found..."
+                                exit 0
+                        fi
+                ;;
+                'false')
+                        echo "Creating a new game."
+                        su -l openttd -c "$execpath -D -x -d ${DEBUG}"
+                        exit 0
+                ;;
+                'last-autosave')
+
+			savegame=${savepath}/autosave/`ls -rt ${savepath}/autosave/ | tail -n1`
+
+			if [ -r ${savegame} ]; then
+	                        echo "Loading ${savegame}"
+        	                su -l openttd -c "$execpath -D -g ${savegame} -x -d ${DEBUG}"
+                	        exit 0
+			else
+				echo "${savegame} not found..."
+	                        echo "Creating a new game."
+	                        su -l openttd -c "$execpath -D -x -d ${DEBUG}"
+	                        exit 0
+			fi
+                ;;
+                'exit')
+
+			savegame="${savepath}/autosave/exit.sav"
+
+			if [ -r ${savegame} ]; then
+	                        echo "Loading ${savegame}"
+        	                su -l openttd -c "$execpath -D -g ${savegame} -x -d ${DEBUG}"
+                	        exit 0
+			else
+				echo "${savegame} not found..."
+				echo "Creating a new game."
+                        	su -l openttd -c "$execpath -D -x -d ${DEBUG}"
+                        	exit 0
+			fi
+                ;;
+		*)
+			echo "ambigous loadgame (\"${loadgame}\") statement inserted."
+			exit 1
+		;;
+        esac
+else
+	echo "\$loadgame (\"${loadgame}\") not set, starting new game"
+        su -l openttd -c "$execpath -D -x"
+        exit 0
+fi
